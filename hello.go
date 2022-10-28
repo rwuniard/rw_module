fda7340872da7461d54a9ba3aa7661983bcd80c5
Ronson Wuniardy <rwuniard@hotmail.com>
2022-10-27 20:28:18 -0400

    updating hello with a new version

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ package rw_module
 import "fmt"
 
 func SayHello() string {
-	message := "Hello this is from rw_module."
+	message := "Hello this is from rw_module. v0.2.0"
 	fmt.Println(message)
 
 	return message
--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSayHello(t *testing.T) {
 	result := SayHello()
-	expected_result := "Hello this is from rw_module."
+	expected_result := "Hello this is from rw_module. v0.2.0"
 
 	if result != expected_result {
 		t.Error("Supposed to get:", expected_result, "but got:", result)
